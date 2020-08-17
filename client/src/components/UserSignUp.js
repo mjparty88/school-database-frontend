82a560f33edcb0ece230649ce64da0061797464e
Matt Partridge <partridgem88@gmail.com>
2020-08-18 09:44:23 +1000

    Updated statechange handlers on the UserSignUp.js form.

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -5,8 +5,47 @@ export default class UserSignUp extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      firstName: null,
+      lastName: null,
+      emailAddress: null,
+      password: null,
+      confirmPassword: null
+    }
   }
+
+  handleFirstNameChange(e) {
+    this.setState({
+      firstName: e.target.value
+    })
+  }
+
+  handleLastNameChange(e) {
+    this.setState({
+      lastName: e.target.value
+    })
+  }
+
+  handleEmailChange(e) {
+    this.setState({
+      emailAddress: e.target.value
+    })
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+      //To future me. I don't like that the user's password is displayed in the React DevTools. Think of something different. Maybe hash each time?
+    })
+  }
+
+  handleConfirmPasswordChange(e) {
+    this.setState({
+      confirmPassword: e.target.value
+      //To future me. I don't like that the user's password is displayed in the React DevTools. Think of something different. Maybe hash each time?
+    })
+  }
+
 
   render() {
 
@@ -17,19 +56,19 @@ export default class UserSignUp extends Component {
           <div>
             <form>
               <div>
-                <input id="firstName" name="firstName" type="text" className placeholder="First Name"></input>
+                <input id="firstName" name="firstName" type="text" onChange={this.handleFirstNameChange.bind(this)} className placeholder="First Name"></input>
               </div>
               <div>
-                <input id="lastName" name="lastName" type="text" className placeholder="First Name"></input>
+                <input id="lastName" name="lastName" type="text" onChange={this.handleLastNameChange.bind(this)} className placeholder="First Name"></input>
               </div>
               <div>
-                <input id="emailAddress" name="emailAddress" type="text" className placeholder="Email Address"></input>
+                <input id="emailAddress" name="emailAddress" type="text" onChange={this.handleEmailChange.bind(this)} className placeholder="Email Address"></input>
               </div>
               <div>
-                <input id="password" name="password" type="password" className placeholder="Password"></input>
+                <input id="password" name="password" type="password" onChange={this.handlePasswordChange.bind(this)} className placeholder="Password"></input>
               </div>
               <div>
-                <input id="confirmPassword" name="confirmPassword" type="password" className placeholder="Confirm Password"></input>
+                <input id="confirmPassword" name="confirmPassword" type="password" onChange={this.handleConfirmPasswordChange.bind(this)} className placeholder="Confirm Password"></input>
               </div>
               <div className="grid-100 pad-bottom">
                 <button className="button" type="submit">Sign Up</button>
