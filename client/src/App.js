c66220920a354bb0a8ee495248efeb06d7e772a7
Matt Partridge <partridgem88@gmail.com>
2020-08-21 17:22:59 +1000

    Updated components to consume context.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import withContext from './Context';
 import Header from './components/Header';
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
@@ -13,6 +14,15 @@ import UserSignUp from './components/UserSignUp'
 import CreateCourse from './components/CreateCourse'
 import UpdateCourse from './components/UpdateCourse'
 
+//turn all components into subscribers
+const HeaderWithContext = withContext(Header)
+const CoursesWithContext = withContext(Courses);
+const CreateCourseWithContext = withContext(CreateCourse);
+const UpdateCourseWithContext = withContext(UpdateCourse);
+const CourseDetailWithContext = withContext(CourseDetail);
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignInWithContext = withContext(UserSignIn);
+const ErrorPageWithContext = withContext(ErrorPage);
 
 
 export default function App() {
@@ -20,15 +30,15 @@ export default function App() {
   return (
     <Router>
       <div>
-      <Header />
+      <HeaderWithContext />
         <Switch>
-          <Route exact path="/" component={Courses} />
-          <Route path="/courses/create" component={CreateCourse}/>
-          <Route path="/courses/:id/update" component={UpdateCourse}/>
-          <Route path="/courses/:id" component={CourseDetail} />
-          <Route path="/signup" component={UserSignUp} />
-          <Route path="/signin" component={UserSignIn}/>
-          <Route component={ErrorPage}/>
+          <Route exact path="/" component={CoursesWithContext} />
+          <Route path="/courses/create" component={CreateCourseWithContext}/>
+          <Route path="/courses/:id/update" component={UpdateCourseWithContext}/>
+          <Route path="/courses/:id" component={CourseDetailWithContext} />
+          <Route path="/signup" component={UserSignUpWithContext} />
+          <Route path="/signin" component={UserSignInWithContext}/>
+          <Route component={ErrorPageWithContext}/>
         </Switch>
       </div>
     </Router>
